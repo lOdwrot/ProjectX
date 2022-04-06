7d19729767b4a4d477628876f1988403f0bd6d54
Danio88 <frasek88@gmail.com>
2022-04-06 23:05:07 +0200

    Project initialization

---
+++ src/components/resource-list/data.js
@@ -0,0 +1,25 @@
+export const resourceSample1 = {
+    name: "Czujnik Cisnienia RTX1025",
+    price: 125.15,
+    imageUrl: "https://a.allegroimg.com/original/0378c8/29aa508c4abead648ef0a717cc3a",
+    attributes: [
+        {
+          name: "Cisnienie",
+          value: 16,
+          unit: "bar",
+        },
+      ],
+}
+
+export const resourceSample2 = {
+    name: "Czujnik Cisnienia RTX1026",
+    price: 145.15,
+    imageUrl: "https://a.allegroimg.com/original/0378c8/29aa508c4abead648ef0a717cc3a",
+    attributes: [
+        {
+          name: "Cisnienie",
+          value: 16,
+          unit: "bar",
+        },
+      ],
+}
